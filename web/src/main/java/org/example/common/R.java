d28c959fcfd3cb4332c0566d8e300523b454cee8
lizihan <lee-zh@foxmail.com>
2025-09-27 21:51:06 +0800

    commit
    (cherry picked from commit 633cdf74d8f7b206f69276d96e213a561820e261)

--- web/src/main/java/org/example/common/R.java
+++ web/src/main/java/org/example/common/R.java
@@ -15,74 +15,14 @@ public class R extends HashMap<String, Object> {
         put("code", STATE_SUCCESS);
     }
 
-    public static R error() {
-        return error(STATE_ERROR, "未知异常，请稍后再试");
-    }
-
     public static R error(String msg) {
         return error(STATE_ERROR, msg);
     }
-
-    public static R DBError(String msg) {
-        return error(401, msg);
-    }
-
-    public static R paramsError(String msg) {
-        return error(400, msg);
-    }
-
-    public static R authenticationError(String msg) {
-        return error(402, msg);
-    }
-
-    public static R DBError() {
-        return error(401, "操作数据库失败！");
-    }
-
-    public static R paramsError() {
-        return error(400, "参数异常");
-    }
-
-    public static R authenticationError() {
-        return error(402, "登录认证失败");
-    }
-
 
     public static R error(int code, String msg) {
         R r = new R();
         r.put("code", code);
         r.put("msg", msg);
-        return r;
-    }
-
-    public static R resultError(String msg) {
-        return error("fail", msg);
-    }
-
-    public static R resultSuccess(String msg) {
-        return error("success", msg);
-    }
-
-    public static R error(String code, String msg) {
-        return ok(code, msg);
-    }
-
-    public static R ok(String code, String msg) {
-        R r = new R();
-        r.put("resultCode", code);
-        r.put("resultMsg", msg);
-        return r;
-    }
-
-    public static R ok(String msg) {
-        R r = new R();
-        r.put("msg", msg);
-        return r;
-    }
-
-    public static R ok(Map<String, Object> map) {
-        R r = new R();
-        r.putAll(map);
         return r;
     }
 
